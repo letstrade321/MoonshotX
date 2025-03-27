05caab0852f054cb785102f21ba995925183c725
letstrade321 <ticketmaster251200@gmail.com>
2025-03-27 15:47:29 +0530

    Add toaster module to rollupOptions.external as suggested by error

--- wallet-sync-game-main/vite.config.ts
+++ wallet-sync-game-main/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
   build: {
     sourcemap: true,
     rollupOptions: {
+      external: ["@/components/ui/toaster"],
       output: {
         manualChunks: undefined
       }
